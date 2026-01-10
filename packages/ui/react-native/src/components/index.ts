ad7ba58ca0c300ce106cbf7f2c677811a33dc4c7
Yanay Zabary <yanay.tsabary@gmail.com>
2026-01-10 10:31:36 +0200

    refactor: remove ui-core pacakges from monorepo

--- packages/ui/react-native/src/components/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as CustomButton } from "./CustomButton";
-export { default as EmojiSuggestions } from "./EmojiSuggestions";
-export { default as FromNow } from "./FromNow";
-export { default as GiphyContainer } from "./GiphyContainer";
-
-export { CommentSkeleton, UserMentionSkeleton, Skeleton } from "./Skeleton";
-export { default as UserAvatar } from "./UserAvatar";
